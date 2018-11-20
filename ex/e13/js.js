05acceab4f4ac93f6698f56b8b5f5e3b994ca4f2
Wesley <wesley.meyer@yahoo.com>
2018-11-20 12:43:59 -0500

    e13

---
+++ ex/e13/js.js
@@ -0,0 +1,43 @@
+$(document).ready(function()
+{
+    guessGame();
+});
+
+function guessGame()
+{
+	var array = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Nidoqueen","Nidoran","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"];	
+	var choose = Math.floor(Math.random() * array.length);
+	var objective = array[choose].toLowerCase();
+	console.log(array[choose]);
+	console.log(objective);
+	var display = choose + 1;
+	$('.num').text(display);
+
+
+	$('#submit').click(function()
+	{
+		var guess = $('input').val();
+		console.log(guess);
+		if (guess == objective)
+		{
+			$('.header').text(guess + ' is correct!');
+			$('input').val('');
+			$('.b').text('play again');
+			$('#submit').click(function()
+			{
+				location.reload(true);
+			});
+		}
+		else
+		{
+			$('.header').text(guess + ' is wrong');
+			$('input').val('');
+			$('.b').text('play again');
+			$('#submit').click(function()
+			{
+				location.reload(true);
+			});		
+		}
+
+	});
+}
